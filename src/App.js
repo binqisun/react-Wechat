22b6cbd25f10f4203a089a49cceb7d16cf7f2250
binqisun <30937912+binqisun@users.noreply.github.com>
2021-11-10 19:53:40 +0800

    add container

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./styles.css";
 import MessageList from "./components/messageList";
+import { Container } from "@mui/material";
 
 export default function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Wechat App</h1>
-      </header>
-      <div>
-        <MessageList />
-      </div>
+      <Container maxWidth="sm">
+        <header>
+          <h1>Wechat App</h1>
+        </header>
+        <div>
+          <MessageList />
+        </div>
+      </Container>
     </div>
   );
 }
--- src/components/systemMessage/systemMessage.js
+++ src/components/systemMessage/systemMessage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./systemMessage.css";
 
 export default function systemMessage() {
   return (
-    <div className="systemMessage">
-      <h2>systemMessage</h2>
+    <div className="systemContent">
+      <p className="systemP">systemMessage</p>
     </div>
   );
 }
